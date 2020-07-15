437144aedb836d77c6160da1cf21c708df5f9755
Mark.Carolan <Mark.Carolan@busyatwork.com.au>
2020-07-15 18:51:51 +1000

    More direct control. Crisper on device as expected.

--- Assets/Landscape/Scripts/TouchController.cs
+++ Assets/Landscape/Scripts/TouchController.cs
@@ -19,52 +19,16 @@ namespace QuiteSensible
 
         private bool cursorVisible;
         private Vector3 targetEuler = Vector3.zero;
-        private int halfScreenWidth, halfScreenHeight;
-        private Vector2 touchDownPos;
-        private Vector2 unitizedFirstTouch = Vector3.zero, unitizedTouchDelta = Vector2.zero;
 
         void Start()
         {
 #if UNITY_EDITOR || UNITY_STANDALONE
             SetCursorVisible(false);
 #endif
-            halfScreenWidth = Screen.width / 2;
-            halfScreenHeight = Screen.height / 2;
         }
 
         void FixedUpdate()
         {
-
-#if xUNITY_EDITOR || UNITY_STANDALONE
-            if (Input.GetKey(KeyCode.Escape))
-                SetCursorVisible(!cursorVisible);
-
-            if (Input.GetMouseButtonUp(0)) // simulate finger off screen
-            {
-                ButtonUp?.Invoke();
-            }
-            else if (Input.GetMouseButtonDown(0))
-            {
-                touchDownPos.x = Input.GetAxis("Mouse X");
-                touchDownPos.y = Input.GetAxis("Mouse Y");                
-            }
-            else if (Input.GetMouseButton(0))
-            {
-                currentDownPos.x = Input.GetAxis("Mouse X");
-                currentDownPos.y = Input.GetAxis("Mouse Y");
-
-                Vector2 deltaPos = currentDownPos - touchDownPos;
-                var currentRot = target.localRotation.eulerAngles;
-
-                if (currentRot.x > 180f) // Adjust to range -min .. +max for clamping. Unity will adjust negatives
-                    currentRot.x -= 360f;
-
-                targetEuler.x = Mathf.Clamp(currentRot.x - deltaPos.y * pitchAngleIncrement * Time.deltaTime, pitchMin, pitchMax);
-                targetEuler.y = currentRot.y + deltaPos.x * yawAngleIncrement * Time.deltaTime;
-
-                target.localRotation = Quaternion.Euler(targetEuler);
-            }
-#else
             if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
@@ -74,36 +38,24 @@ namespace QuiteSensible
                 if (currentRot.x > 180f) // Adjust to range -min .. +max for clamping. Unity will adjust negatives
                     currentRot.x -= 360f;
 
-                Vector2 touchDelta = touch.position - touchDownPos;                
-
                 switch (touch.phase)
                 {
                     case TouchPhase.Stationary:                        
                         break;
                     case TouchPhase.Began:
-                        unitizedFirstTouch.x = (touch.position.x - halfScreenWidth) / halfScreenWidth;
-                        unitizedFirstTouch.y = (touch.position.y - halfScreenHeight) / halfScreenHeight;
                         break;
                     case TouchPhase.Ended:
-                        unitizedTouchDelta = Vector2.zero;
                         ButtonUp?.Invoke();
                         break;
                     case TouchPhase.Moved:
-                        var unitX = (touch.position.x - halfScreenWidth) / halfScreenWidth;
-                        var unitY = (touch.position.y - halfScreenHeight) / halfScreenHeight;
-                        unitizedTouchDelta.x = unitX - unitizedFirstTouch.x;
-                        unitizedTouchDelta.y = unitY - unitizedFirstTouch.y;
                         break;
                 }
                 
-                targetEuler.x = Mathf.Clamp(currentRot.x - unitizedTouchDelta.y * pitchAngleIncrement * Time.deltaTime, pitchMin, pitchMax);
-                targetEuler.y = currentRot.y + unitizedTouchDelta.x * yawAngleIncrement * Time.deltaTime;
-
-                gameData.PlayerUiText(string.Format("unitizedTouchDelta.x: {0}, unitizedTouchDelta.y: {1}", unitizedTouchDelta.x, unitizedTouchDelta.y));
+                targetEuler.x = Mathf.Clamp(currentRot.x - touch.deltaPosition.y * pitchAngleIncrement * Time.deltaTime, pitchMin, pitchMax);
+                targetEuler.y = currentRot.y + touch.deltaPosition.x * yawAngleIncrement * Time.deltaTime;
 
                 target.localRotation = Quaternion.Euler(targetEuler);
             }
-#endif
         }
 
         /// <summary>
